f684b9abf0d1d5729abff63961e3c37deb4ad219
DarrenKent <darrennkent@gmail.com>
2013-09-10 23:44:28 -0600

    Added full playability.

---
+++ src/Disk.py
@@ -0,0 +1,62 @@
+#----------------------------------
+#
+#	Towers of Hanoi
+#	By: Darren Kent
+#
+#	Disk.py
+#	This Class Handles Disk Objects
+#
+#----------------------------------
+
+import os
+import pygame
+import Button
+
+class Disk ( Button.Button ):
+	def __init__( self , diskId , row , column , image , screen , buttonHandler ):
+		self.DiskImage = pygame.image.load( os.path.join( os.path.join( 'data' , 'textures' ) , image ))
+		Button.Button.__init__( self , 
+						screen.get_width() / 2 - 236 + 236 * column - self.DiskImage.get_width() / 2 , 
+						screen.get_height() - 95 - 35 * (row) , 
+						diskId , image , image , buttonHandler )
+		
+		self.NewX = self.X
+		self.NewY = self.Y
+		self.Column = column
+		self.Row = row
+		self.Screen = screen
+		self.MouseXDist = 0
+		self.MouseYDist = 0
+		
+		
+	def DrawButton( self ):
+		self.Screen.blit( self.DiskImage , ( self.NewX , self.NewY ))
+	
+	def SetMouseHover( self , mouseX , mouseY ):
+		if( mouseX > self.NewX and mouseX < self.NewX + self.Width ):
+			if( mouseY > self.NewY and mouseY < self.NewY + self.Height ):
+				self.Hover = True
+				return
+		self.Hover = False
+		
+	def SetLocation( self , colPlacement , rowPlacement ):
+		tX , tY = pygame.mouse.get_pos()
+		newColumn = 0
+		if( tX > self.Screen.get_width() / 2 - 236 - 117 and tX < self.Screen.get_width() / 2 - 236 + 117 ):
+			newColumn = 0
+			newX = self.Screen.get_width() / 2 - 236 - self.DiskImage.get_width() / 2
+		elif( tX > self.Screen.get_width() / 2 - 117 and tX < self.Screen.get_width() / 2 + 117 ):
+			newColumn = 1
+			newX = self.Screen.get_width() / 2 - self.DiskImage.get_width() / 2
+		elif( tX > self.Screen.get_width() / 2 + 236 - 117 and tX < self.Screen.get_width() / 2 + 236 + 117 ):
+			newColumn = 2
+			newX = self.Screen.get_width() / 2 + 236 - self.DiskImage.get_width() / 2
+			
+		if( len(rowPlacement[newColumn]) == 0 or self.ButtonId < min(rowPlacement[newColumn])):
+			self.Column = newColumn
+			self.Row = colPlacement[self.Column]+1
+			self.X = newX
+			self.Y = self.Screen.get_height() - 95 - 35 * (self.Row)
+			
+		self.NewX = self.X
+		self.NewY = self.Y
--- src/PlayState.py
+++ src/PlayState.py
@@ -10,9 +10,11 @@
 
 import os
 import pygame
+import time
 from State import *
 import Button
-import time
+import Disk
+
 
 class PlayState( State ):
 	def __init__( self , screen , gameController , numDisks ):
@@ -26,6 +28,17 @@ class PlayState( State ):
 		self.StartTime = time.time()
 		self.StartPause = 0
 		self.TimePaused = 0
+		self.Towers = [0,0,0]
+		self.TowersList = [[],[],[]]
+		self.Disks = []
+		self.WinTime = 0
+		self.Win = False
+		for disk in range( numDisks ):
+			tNewDisk = Disk.Disk( disk , numDisks - disk , 0 , "disk_"+str(disk+1)+"_red.png" , screen , self.DiskHandler )
+			self.Disks.append( tNewDisk )
+			self.Towers[0] += 1
+			self.TowersList[0].append(disk)
+		self.SelectedDisk = None
 		
 		# Initialize Background Images
 		self.Background = pygame.image.load( os.path.join( os.path.join( 'data' , 'textures' ) , self.Texture+'_background.png' ))
@@ -46,22 +59,38 @@ class PlayState( State ):
 		self.Buttons.append( Button.Button( self.Screen.get_width() / 2 - 75 , self.Screen.get_height() / 2 + self.MenuBackgroundOverlay.get_width() / 2 - 180 , 'Quit' , 'button_quit_standard.png' , 'button_quit_hover.png' , self.ButtonHandler ))
 		
 	def ExecuteStateLogic( self , KeysHeld , KeysPressed , clock ):
+		if( not pygame.mouse.get_pressed()[0] and self.SelectedDisk ):
+			self.SelectedDisk.SetLocation( self.Towers , self.TowersList )
 		if( not pygame.mouse.get_pressed()[0] ):
 			self.MouseReleased = True
-			self.StartSlider = False
+			self.SelectedDisk = None
 			
 		if( self.Countdown ):
 			self.ExecuteCountdown()
+		elif( self.Win ):
+			self.ExecuteWin()
 		elif( self.Pause ):
 			self.ExecutePauseMenuLogic()
 		else:
 			self.ExecuteGameLogic()
 			
 	def ExecuteGameLogic( self ):
+		self.Towers = [0,0,0]
+		self.TowersList = [[],[],[]]
 		tX , tY = pygame.mouse.get_pos()
 		self.Buttons[0].SetMouseHover( tX , tY )
+		for disk in self.Disks:
+			disk.SetMouseHover( tX , tY )
+			self.Towers[disk.Column] += 1
+			self.TowersList[disk.Column].append(disk.ButtonId)
+			
 		if( self.Buttons[0].IsMouseInside() and pygame.mouse.get_pressed()[0] and self.MouseReleased ):
 			self.Buttons[0].ExecuteButton()
+		
+		for disk in self.Disks:
+			disk.ExecuteButton()
+			
+		self.CheckForWin()
 		
 			
 	def ExecutePauseMenuLogic( self ):
@@ -76,6 +105,17 @@ class PlayState( State ):
 	def ExecuteCountdown( self ):
 		self.Countdown = False
 		
+	def ExecuteWin( self ):
+		tX , tY = pygame.mouse.get_pos()
+		self.Buttons[2].SetMouseHover( tX , tY )
+		if( self.Buttons[2].IsMouseInside() and pygame.mouse.get_pressed()[0] and self.MouseReleased ):
+			self.Buttons[2].ExecuteButton()
+			
+	def CheckForWin( self ):
+		if( self.Towers[2] == self.NumDisks ):
+			self.WinTime = time.time() - self.StartTime - self.TimePaused
+			self.Win = True
+
 	def DrawStateFrame( self , screen , clock ):
 		screen.fill( (0,253,251) )
 		
@@ -90,7 +130,12 @@ class PlayState( State ):
 		
 		self.Buttons[0].DrawButton( self.Screen )
 		
-		if( self.Pause ):
+		for disk in self.Disks:
+			disk.DrawButton()
+			
+		if( self.Win ):
+			self.DrawWin()
+		elif( self.Pause ):
 			self.DrawPauseMenu()
 			timer = self.Text.render( "PAUSED" , 1 , (255,255,255) )
 			self.Screen.blit( timer , ( 10 , 10) )
@@ -98,12 +143,26 @@ class PlayState( State ):
 			timer = self.Text.render( "Time: %.2f" % (time.time() - self.StartTime - self.TimePaused) , 1 , (255,255,255) )
 			self.Screen.blit( timer , ( 10 , 10) ) 
 			
+		
+			
 	def DrawPauseMenu( self ):
 		rect = pygame.Surface( ( self.Screen.get_width() , self.Screen.get_height() ) , pygame.SRCALPHA , 32 )
 		rect.fill( (0,0,0,200) )
 		self.Screen.blit( rect , (0,0) )
 		self.Screen.blit( self.MenuBackgroundOverlay , ( self.Screen.get_width() / 2 - self.MenuBackgroundOverlay.get_width() / 2 , self.Screen.get_height() / 2 - self.MenuBackgroundOverlay.get_height() / 2 ))
 		self.Buttons[1].DrawButton( self.Screen )
+		self.Buttons[2].DrawButton( self.Screen )
+		
+	def DrawWin( self ):
+		rect = pygame.Surface( ( self.Screen.get_width() , self.Screen.get_height() ) , pygame.SRCALPHA , 32 )
+		rect.fill( (0,0,0,200) )
+		self.Screen.blit( rect , (0,0) )
+		self.Screen.blit( self.MenuBackgroundOverlay , ( self.Screen.get_width() / 2 - self.MenuBackgroundOverlay.get_width() / 2 , self.Screen.get_height() / 2 - self.MenuBackgroundOverlay.get_height() / 2 ))
+		
+		winText = self.Text.render( "Congratulations, You Win!" , 1 , (255,255,255) )
+		winTimeText = self.Text.render( "Time: %.2f" % self.WinTime , 1 , (255,255,255) )
+		self.Screen.blit( winText , ( self.Screen.get_width() / 2 - winText.get_width() / 2 , self.Screen.get_height() / 2 - self.MenuBackgroundOverlay.get_width() / 2 + 20 ))
+		self.Screen.blit( winTimeText , ( self.Screen.get_width() / 2 - winTimeText.get_width() / 2 , self.Screen.get_height() / 2 - self.MenuBackgroundOverlay.get_width() / 2 + 60 ))
 		self.Buttons[2].DrawButton( self.Screen )
 		
 	def ButtonHandler( self , button ):
@@ -116,3 +175,14 @@ class PlayState( State ):
 			self.TimePaused += time.time() - self.StartPause
 		elif( button.ButtonId == 'Quit' ):
 			self.StateQuit = True
+			
+	def DiskHandler( self , disk ):
+		tX , tY = pygame.mouse.get_pos()
+		if( disk.IsMouseInside() and pygame.mouse.get_pressed()[0] and self.MouseReleased and (disk.ButtonId) == min(self.TowersList[disk.Column]) ):
+			self.SelectedDisk = disk
+			self.MouseReleased = False
+			disk.MouseXDist = tX - disk.X
+			disk.MouseYDist = tY - disk.Y
+		elif( disk == self.SelectedDisk and not self.MouseReleased ):
+			disk.NewX = tX - disk.MouseXDist
+			disk.NewY = tY - disk.MouseYDist
